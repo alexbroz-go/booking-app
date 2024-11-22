00d6226b086451b7f73f8d718f6de12989028834
alexbroz-go <alex.aksenov.work675@gmail.com>
2024-11-22 12:08:40 +0300

    добавили цикл For

--- main.go
+++ main.go
@@ -1,32 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets int = 50
+	bookings := []string{}
+
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total %v of tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
-	// ask user for their name
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets int
 
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
+		// ask user for their name
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your email name")
-	fmt.Scan(&email)
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your email name")
+		fmt.Scan(&email)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names of bookings are : %v\n", firstNames)
+
+	}
 }
